c137008a68027f19ddae5cbe59880680a8926ebf
Jasmine <hellooworld.warner@gmail.com>
2021-07-20 19:04:42 +1000

    Adding Missing Files Outside of Main Course

---
+++ Challenges/ChallengeModule9.1.js
@@ -0,0 +1,51 @@
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Sculz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+//Question One
+const players0 = game.players[0];
+const players1 = game.players[1];
+
+console.log(players0);
+console.log(players1);
+
+//Question Two
+const gk = players0[0];
+// const [...fieldPlayers] = players[0];
